b83d576daae0c674a8c1b45ff7bfde28df205ef8
Michael Berkovich <michael@translationexchange.com>
2015-09-12 23:38:12 -0700

    fixed samples

--- welp-express-ejs/app.js
+++ welp-express-ejs/app.js
@@ -15,16 +15,43 @@ app.use(express.static(__dirname + '/public'));
 
 // Tml initialization
 app.use(tml.init({
-  key:    "9f319334e0b9d2a902bf99a7994cfc9a77c071ce5511af600d66917f884e6418",
-  token:  "b14a58c1b89030b712f090f2f8d9644b2767ca25ab01b73abef5711d333b14fa",
-  host:   "http://localhost:3000",
+
+  key:    "ced4ac9a2e16bc7993a6f4f5cb8309c62d8cda56f1c801ccb507247f6e3d0b06",
+  token:  "6f8faf23621a79fb534c4be2870cb3ea39a0e3950c15380d856125579cc33457",
+  host:   "https://staging-api.translationexchange.com",
+
+  agent: {
+    enabled:  true,
+    type:     "tools"
+  },
+
+  //cache: {
+  //  adapter: 'memcache',
+  //  hosts: ["localhost:11211"],
+  //  namespace: "ced4ac9a2e16bc7993a6f4f5cb8309c62d8cda56f1c801ccb507247f6e3d0b06"
+  //},
+
+  //key:    "9f319334e0b9d2a902bf99a7994cfc9a77c071ce5511af600d66917f884e6418",
+  //token:  "b14a58c1b89030b712f090f2f8d9644b2767ca25ab01b73abef5711d333b14fa",
+  //host:   "http://localhost:3000",
   debug:  true,
 
-  cache: {
-    adapter: 'memcache',
-    hosts: ["localhost:11211"],
-    namespace: "0d945971"
-  },
+  //cache: {
+  //  adapter: 'memcache',
+  //  hosts: ["localhost:11211"],
+  //  namespace: "0d945971"
+  //},
+
+  //agent: {
+  //  enabled:  true,
+  //  type:     "tools",
+  //  version:  "0.1.10",
+  //  domains:  {
+  //    api:        "http://localhost:3000",
+  //    tools:      "http://localhost:3002",
+  //    analytics:  "https://analyst.translationexchange.com"
+  //  }
+  //},
 
   hook: {
     enabled: true,
@@ -33,6 +60,7 @@ app.use(tml.init({
     //  return true;
     //}
   }
+
 
   //cache: {
   //  adapter: "file",
