66fe91dd1c9b4ea35849b4c4e39fe30e217ee74c
Michael Berkovich <michael@translationexchange.com>
2015-12-15 16:53:26 -0800

    removed unused settings

--- foody-express-jade/app.js
+++ foody-express-jade/app.js
@@ -25,9 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(tml.init({
   // lets switch to staging - i will make the app open
   //host:   "https://staging-api.translationexchange.com",
-  //key:    "f28dd49115a8d386966f63d369c923e76a06cd2c4c34cc157f00e790ef4e2427",
-  //token:  "425000119f70bc555b67c6477010a28a0bbb3ecec2f52c75d436d5b68192b6b9",
-  //debug:  true,
+  key:    "f28dd49115a8d386966f63d369c923e76a06cd2c4c34cc157f00e790ef4e2427",
+  token:  "425000119f70bc555b67c6477010a28a0bbb3ecec2f52c75d436d5b68192b6b9",
+  debug:  true,
   //
   //agent: {
   //  enabled:  true,
@@ -46,25 +46,22 @@ app.use(tml.init({
   //},
 
   // Michael's Laptop
-  key: "05afee35f0450c8a5ca5fe4d29e207324b1cf5ac423e909b6f0f980976b95bff",
-  token: "a5096b1a5b27072c5c55d34c9729d5cb54f12e8eb49637a56e49b817bdb5b582",
-  host: "http://localhost:3000",
-  debug:  true,
+  //key: "05afee35f0450c8a5ca5fe4d29e207324b1cf5ac423e909b6f0f980976b95bff",
+  //token: "a5096b1a5b27072c5c55d34c9729d5cb54f12e8eb49637a56e49b817bdb5b582",
+  //host: "http://localhost:3000",
+  //debug:  true,
 
-  agent: {
-    enabled:  true,
-    type:     "tools",
-    version:  "0.1.7",
-    domains:  {
-      api:   "http://localhost:3000",
-      tools: "http://localhost:3002"
-    }
-  },
+  //cache: {
+  //  adapter: "memcache",
+  //  hosts: ["localhost:11211"],
+  //  namespace: "05afee35f0450c8a5ca5fe4d29e207324b1cf5ac423e909b6f0f980976b95bff"
+  //}
 
   cache: {
-    adapter: "memcache",
-    hosts: ["localhost:11211"],
-    namespace: "05afee35f0450c8a5ca5fe4d29e207324b1cf5ac423e909b6f0f980976b95bff"
+    adapter: "redis",
+    host: "localhost",
+    port: 6379,
+    namespace: "f28dd49115a8d386966f63d369c923e76a06cd2c4c34cc157f00e790ef4e2427"
   }
 
   //current_locale: 'fr',
